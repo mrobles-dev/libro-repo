a8317241ebe74e929b87acded41cb8e2d795fdde
mrobles-dev <miltonjrobles1996@gmail.com>
2023-06-27 01:05:20 -0500

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://miltonjrobles1996:<password>@cluster0.jlluofa.mongodb.net/googlebooks"
+    "mongodb+srv://miltonjrobles1996:rox@cluster0.jlluofa.mongodb.net/googlebooks"
 );
 
 module.exports = mongoose.connection;
