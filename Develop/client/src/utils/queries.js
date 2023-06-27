c9de0b4f8219042a6ebc6628a9b761cab1440745
mrobles-dev <miltonjrobles1996@gmail.com>
2023-06-26 22:45:33 -0500

    added { to me

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 
-export const GET_ME = gql`
+export const GET_ME = gql`{
  me {
       _id
       username
@@ -15,4 +15,4 @@ savedBooks {
         link
       }
     }
-} `;
+ }`;
