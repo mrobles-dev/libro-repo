dc4b11008c20c369411972c0364965ec2ecb0a42
mrobles-dev <miltonjrobles1996@gmail.com>
2023-06-26 21:08:24 -0500

    debugging

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import{ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import{ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
